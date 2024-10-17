20dba740fa5280b0b52ca1abfd118d81ac44045a
Misakiop <atianyar@qq.com>
2024-10-17 17:23:12 +0800

    项目构建

---
+++ src/main/java/cn/lmu/candy/CandyBackendApplication.java
@@ -0,0 +1,13 @@
+package cn.lmu.candy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CandyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/lmu/candy/CandyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lmu.candy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
