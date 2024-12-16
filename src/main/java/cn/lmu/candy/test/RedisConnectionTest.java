5c93b3714fb75ecfe5a4c71eae85b212ca611bf1
Misakiop <atianyar@qq.com>
2024-12-16 21:25:23 +0800

    完成Redis相关功能

--- src/main/java/cn/lmu/candy/test/RedisConnectionTest.java
+++ src/main/java/cn/lmu/candy/test/RedisConnectionTest.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
+/**
+ * Redis测试类
+ */
 @Component
 public class RedisConnectionTest {
     @Autowired
